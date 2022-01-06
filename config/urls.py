27954f97fe0e9e193652e1b120a1f8cc2fb14128
joe <48598137+joeliberal@users.noreply.github.com>
2022-01-06 16:51:28 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,16 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('movie.urls')),
     path('api/', include('api.urls')),
-
-]
+)
 
 if settings.DEBUG:
     urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
